fb2c5238e8204df73479fd08c8bb0d3c48ff12c7
Krisna Anugrah Arianto Heru Putro <5053241002@student.its.ac.id>
2026-09-08 17:27:48 +0700

    pertemuan-02: level 5 selesai

--- pertemuan-02/main.go
+++ pertemuan-02/main.go
@@ -37,7 +37,10 @@ func HitungDiskon(total float64) float64 {
 }
 
 func TotalSetelahDiskon(qty []int, hargaSatuan []float64, tarifPajak float64) float64 {
-	panic("belum diimplementasikan")
+	total := HitungTotalPesanan(qty, hargaSatuan)
+	totalSetelahPajak := TerapkanPajak(total, tarifPajak)
+	diskon := HitungDiskon(totalSetelahPajak)
+	return totalSetelahPajak - diskon
 }
 
 func ValidasiPesanan(qty []int, hargaSatuan []float64) (bool, string) {
