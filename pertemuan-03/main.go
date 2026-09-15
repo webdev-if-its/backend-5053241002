a467b50938c0cb6c3bd2fa09c77dc8f7b00105ec
Krisna Anugrah Arianto Heru Putro <5053241002@student.its.ac.id>
2026-09-15 17:31:16 +0700

    pertemuan-03: level 6 selesai

--- pertemuan-03/main.go
+++ pertemuan-03/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // TODO(Level 1): lihat SOAL.md untuk kontrak lengkap tiap fungsi di bawah.
@@ -33,6 +34,10 @@ type TokoTugas struct {
 }
 
 func TambahTugas(toko *TokoTugas, judul string) (Task, error) {
+	if strings.TrimSpace(judul) == "" {
+		return Task{}, ErrInputKosong
+	}
+
 	toko.NextID++
 	tugas := Task{
 		ID:      toko.NextID,
@@ -41,7 +46,6 @@ func TambahTugas(toko *TokoTugas, judul string) (Task, error) {
 	}
 	toko.Daftar = append(toko.Daftar, tugas)
 	return tugas, nil
-
 }
 
 func LihatTugas(toko *TokoTugas, id int) (Task, error) {
